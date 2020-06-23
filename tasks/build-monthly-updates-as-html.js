9f8b1868494248b5a058b4482641a4d0899b4762
ahocquard <alexandre.hocquard@akeneo.com>
2020-06-23 12:03:33 +0200

    GITHUB-APD-168: generate updates of the previous month in production

--- tasks/build-monthly-updates-as-html.js
+++ tasks/build-monthly-updates-as-html.js
@@ -65,9 +65,11 @@ md
 gulp.task('build-monthly-updates-as-html', ['clean-dist','less'], function() {
     const fileDirectorySource = 'content/updates';
     const fileDirectoryDestination = './dist/pim/serenity/updates';
+    // by default, we generate all updates except if env variable ONLY_PREVIOUS_MONTH_UPDATES=true
+    const generateAllUpdates = !(process.env.ONLY_PREVIOUS_MONTH_UPDATES && process.env.ONLY_PREVIOUS_MONTH_UPDATES === 'true');
 
     return merge(
-        generateUpdates(fileDirectorySource, fileDirectoryDestination),
+        generateUpdates(fileDirectorySource, fileDirectoryDestination, generateAllUpdates),
         generateIndex(fileDirectorySource, fileDirectoryDestination)
     );
 });
@@ -89,14 +91,24 @@ function generateIndex(fileDirectorySource, fileDirectoryDestination) {
         .pipe(gulp.dest(fileDirectoryDestination));
 };
 
-function generateUpdates(fileDirectorySource, fileDirectoryDestination) {
+function generateUpdates(fileDirectorySource, fileDirectoryDestination, generateAllUpdates) {
 
     const rawData = fs.readFileSync(fileDirectorySource + '/index.json');
     const monthlyUpdates = JSON.parse(rawData);
 
-    var folders = getFolders(fileDirectorySource);
+    const folders = getFolders(fileDirectorySource).filter((folder) => {
+        const currentDate = new Date(Date.now());
+        const dayOfMonth = currentDate.getDate();
+        const previousMonthDate = dayOfMonth < 5 ? new Date(currentDate.setMonth(currentDate.getMonth() - 2)) : new Date(currentDate.setMonth(currentDate.getMonth() - 1));
 
-    var tasks = folders.map(function (folder) {
+        const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(previousMonthDate);
+        const month = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(previousMonthDate);
+        const maxDate = year + '-' + month;
+
+        return folder <= maxDate || generateAllUpdates;
+    });
+
+    const tasks = folders.map(function (folder) {
         return gulp.src(path.join(fileDirectorySource, folder, '/**/*.md'))
             .pipe(frontMatter({property: 'fm', remove: true}))
             .pipe(concat(folder + '-copy.html'))
--- tests/build-monthly-updates-as-html.test.js
+++ tests/build-monthly-updates-as-html.test.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 test('Generate the static html about the updates of the PIM', done => {
     del(['tmp/*']);
 
-    const stream = generateUpdates('./tests/updates/nominal_case/', './tmp');
+    const stream = generateUpdates('./tests/updates/nominal_case/', './tmp', false);
 
     stream.on('finish', () => {
         const expectedData = fs.readFileSync('./tests/updates/nominal_case/expected_2020-02.html');
@@ -13,6 +13,83 @@ test('Generate the static html about the updates of the PIM', done => {
 
         try {
             expect(data.toString()).toStrictEqual(expectedData.toString());
+            done();
+        } catch (error) {
+            done(error);
+        }
+    });
+});
+
+/**
+ * The March month is not finished, so the updates are still written by the PM and not ready to be published *in production*.
+ * Before the 05, it does not generate the update for february, as it's waiting for validation.
+ */
+test('When we are from 05 to 30 March 2020, generate the updates until February 2020', done => {
+    del(['tmp/*']);
+    jest
+        .spyOn(global.Date, 'now')
+        .mockImplementation(() =>
+            new Date('2020-03-05T00:00:00.000Z').valueOf()
+        );
+
+    const stream = generateUpdates('./tests/updates/nominal_case/', './tmp', false);
+
+    stream.on('finish', () => {
+        try {
+            expect(fs.existsSync('./tmp/2020-02.html')).toBeTruthy();
+            expect(fs.existsSync('./tmp/2020-03.html')).toBeFalsy();
+            done();
+        } catch (error) {
+            done(error);
+        }
+    });
+});
+
+/**
+ * The March month is finished, but it needs validation from the product marketing before publishing it *in production* automatically the 5th of the month.
+ */
+test('When we are from 01 to 04 April 2020, generate the updates until February 2020', done => {
+    del(['tmp/*']);
+    jest
+        .spyOn(global.Date, 'now')
+        .mockImplementation(() =>
+            new Date('2020-04-04T00:00:00.000Z').valueOf()
+        );
+
+    const stream = generateUpdates('./tests/updates/nominal_case/', './tmp', false);
+
+    stream.on('finish', () => {
+        try {
+            expect(fs.existsSync('./tmp/2020-02.html')).toBeTruthy();
+            expect(fs.existsSync('./tmp/2020-03.html')).toBeFalsy();
+            done();
+        } catch (error) {
+            done(error);
+        }
+    });
+});
+
+/**
+ * By default, it generates all updates to test the render in local or in staging. It's only when ONLY_PREVIOUS_MONTH_UPDATES environment variable is set
+ * that it doesn't publish everything.
+ *
+ * ONLY_PREVIOUS_MONTH_UPDATES is set in production as it should not publish on-going updates.
+ */
+test('it generate all updates by default to deploy everything in staging or in local environment', done => {
+    del(['tmp/*']);
+    jest
+        .spyOn(global.Date, 'now')
+        .mockImplementation(() =>
+            new Date('2020-04-04T00:00:00.000Z').valueOf()
+        );
+
+    const generateAllUpdates = true;
+    const stream = generateUpdates('./tests/updates/nominal_case/', './tmp', generateAllUpdates);
+
+    stream.on('finish', () => {
+        try {
+            expect(fs.existsSync('./tmp/2020-02.html')).toBeTruthy();
+            expect(fs.existsSync('./tmp/2020-03.html')).toBeTruthy();
             done();
         } catch (error) {
             done(error);
